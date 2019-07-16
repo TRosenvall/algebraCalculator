274d0abd822d1bef30e346ac33168a4581d83da2
Timothy Rosenvall <timothy.rosenvall@gmail.com>
2019-07-16 15:00:00 -0600

    first commit

---
+++ AlgebraCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlgebraCalculator
+//
+//  Created by Timothy Rosenvall on 7/16/19.
+//  Copyright © 2019 Timothy Rosenvall. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
